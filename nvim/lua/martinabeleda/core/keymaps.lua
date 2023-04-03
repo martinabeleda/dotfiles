171e5f968d446ba0b017e57f6015219c227fd625
martinabeleda <martin.abeleda@gmail.com>
2023-04-02 20:31:33 -0700

    Update treesitter packages

--- nvim/lua/martinabeleda/core/keymaps.lua
+++ nvim/lua/martinabeleda/core/keymaps.lua
@@ -7,7 +7,7 @@ local keymap = vim.keymap
 -- General Keymaps
 ---------------------
 
-keymap.set("i", "jk", "<ESC>") -- use jk to exit insert mode  
+keymap.set("i", "jk", "<ESC>") -- use jk to exit insert mode
 
 keymap.set("n", "<leader>nh", ":nohl<CR>") -- clear search highlight
 
@@ -16,8 +16,8 @@ keymap.set("n", "x", '"_x') -- delete single character without copy
 keymap.set("n", "<leader>+", "<C-a>") -- increment number
 keymap.set("n", "<leader>-", "<C-x>") -- decrement number
 
-keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically 
-keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally 
+keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width
 keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
 
@@ -25,7 +25,6 @@ keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
 keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
 keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
 keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
-
 
 ----------------------
 -- Plugin Keybinds
@@ -42,4 +41,4 @@ keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files wit
 keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
-keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags 
+keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
