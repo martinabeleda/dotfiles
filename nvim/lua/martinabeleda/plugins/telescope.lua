230a6a8cf212d7a08dff40f3b98095c57c7cfa95
martinabeleda <martin.abeleda@gmail.com>
2023-04-25 12:39:26 -0700

    move telescope bindings to correct place

--- nvim/lua/martinabeleda/plugins/telescope.lua
+++ nvim/lua/martinabeleda/plugins/telescope.lua
@@ -1,25 +1,31 @@
 local telescope_setup, telescope = pcall(require, "telescope")
 if not telescope_setup then
-    return
+	return
 end
 
 local actions_setup, actions = pcall(require, "telescope.actions")
 if not actions_setup then
-    return
+	return
 end
 
 -- configure telescope
 telescope.setup({
-  -- configure custom mappings
-  defaults = {
-    mappings = {
-      i = {
-        ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-        ["<C-j>"] = actions.move_selection_next, -- move to next result
-        ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
-      },
-    },
-  },
+	-- configure custom mappings
+	defaults = {
+		mappings = {
+			i = {
+				["<C-k>"] = actions.move_selection_previous, -- move to prev result
+				["<C-j>"] = actions.move_selection_next, -- move to next result
+				["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
+			},
+		},
+	},
 })
 
 telescope.load_extension("fzf")
+
+vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
+vim.keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
+vim.keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
+vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
+vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
